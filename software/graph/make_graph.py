2b48c4afd7b77b736fe28b523d5c07ea2cf13ee2
Olaf Lüke <olaf@tinkerforge.com>
2013-08-30 15:41:39 +0200

    value to frquency graph for new capacitor and resistor values

--- software/graph/make_graph.py
+++ software/graph/make_graph.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-CONFIDENCE = 0.8
+CONFIDENCE = 0.7
 
 def value_to_frequency(value):
-    return 1/(1.4*value*6*(10**-9)*1000)
+    return 1/(1.4*value*5.6*(10**-9)*1000)
 
 y = []
 y_low = []
 y_high = []
 x = []
-for i in range (17, 218):
+for i in range(18, 219):
     value_y = value_to_frequency(i)
     y.append(value_y)
     y_low.append(value_y*CONFIDENCE)
     y_high.append(value_y*(2-CONFIDENCE))
 
-    x.append((i-17)*(512.00)/(217-17))
+    x.append(512 - (i-18)*(512.00)/(218-18))
+
+
+print y[0], y[-1]
 
 fig, ax = plt.subplots(1, sharex=True, sharey=True)
 
@@ -29,7 +32,7 @@ b1 = plt.bar(np.array([1,2])-.2,
              color='b',
              bottom=np.array([10,8])-np.array([2,1]),
              alpha=0.3) 
-plt.legend([b1[0]], ['Confidence (80%)'], shadow=True, fancybox=True, numpoints=1) 
+plt.legend([b1[0]], ['Confidence (70%)'], shadow=True, fancybox=True, numpoints=1, loc=2) 
 
 plt.axis([0, 512, 0, y[0]])
 plt.ylabel('Frequency (Hz)')
