35491c7f8228f1e41a490cdc98d05d43f68038e4
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-10 17:12:14 +0100

    perl: fixed namespace

--- software/examples/perl/example_beep.pl
+++ software/examples/perl/example_beep.pl
@@ -7,8 +7,8 @@ use constant HOST => 'localhost';
 use constant PORT => 4223;
 use constant UID => 'xyz'; # Change to your UID
 
-my $ipcon = IPConnection->new(); # Create IP connection
-my $ps = BrickletPiezoSpeaker->new(&UID, $ipcon); # Create device object
+my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
+my $ps = Tinkerforge::BrickletPiezoSpeaker->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
