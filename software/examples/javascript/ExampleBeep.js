9aab43bb8531d9f82d0c29f364808f3d8db6d50f
Matthias Bolte <matthias@tinkerforge.com>
2014-02-28 19:21:53 +0100

    javascript: Break long lines and cleanup whitespace

--- software/examples/javascript/ExampleBeep.js
+++ software/examples/javascript/ExampleBeep.js
@@ -2,22 +2,22 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = 'iMv';// Change to your UID
+var UID = 'iMv'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var ps = new Tinkerforge.BrickletPiezoSpeaker(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var ps = new Tinkerforge.BrickletPiezoSpeaker(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
-        //Make 2 second beep with a frequency of 1kHz
-        ps.beep(2000, 1000);      
+        // Make 2 second beep with a frequency of 1kHz
+        ps.beep(2000, 1000);
     }
 );
 
@@ -28,4 +28,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
--- software/examples/javascript/ExampleMorseCode.js
+++ software/examples/javascript/ExampleMorseCode.js
@@ -2,22 +2,22 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = 'iMv';// Change to your UID
+var UID = 'iMv'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var ps = new Tinkerforge.BrickletPiezoSpeaker(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var ps = new Tinkerforge.BrickletPiezoSpeaker(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Morse SOS with a frequency of 2kHz
-        ps.morseCode('... --- ...', 2000);      
+        ps.morseCode('... --- ...', 2000);
     }
 );
 
@@ -28,4 +28,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
