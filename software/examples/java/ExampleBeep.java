27442ae15d0b61af6d2fcd80fbbf26e00da1bde5
Matthias Bolte <matthias@tinkerforge.com>
2014-04-02 17:30:09 +0200

    java: Unify constants style

--- software/examples/java/ExampleBeep.java
+++ software/examples/java/ExampleBeep.java
@@ -2,8 +2,8 @@ import com.tinkerforge.BrickletPiezoSpeaker;
 import com.tinkerforge.IPConnection;
 
 public class ExampleBeep {
-	private static final String host = "localhost";
-	private static final int port = 4223;
+	private static final String HOST = "localhost";
+	private static final int PORT = 4223;
 	private static final String UID = "ABC"; // Change to your UID
 
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
@@ -12,7 +12,7 @@ public class ExampleBeep {
 		IPConnection ipcon = new IPConnection(); // Create IP connection
 		BrickletPiezoSpeaker ps = new BrickletPiezoSpeaker(UID, ipcon); // Create device object
 
-		ipcon.connect(host, port); // Connect to brickd
+		ipcon.connect(HOST, PORT); // Connect to brickd
 		// Don't use device before ipcon is connected
 
 		// Make 2 second beep with a frequency of 1kHz
