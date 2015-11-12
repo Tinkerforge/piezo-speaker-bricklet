8dc13cbee8e6d8f95187456f8faebb73ed47a242
Matthias Bolte <matthias@tinkerforge.com>
2015-11-12 16:02:05 +0100

    matlab: Fix callback handling for R2014a
    http://undocumentedmatlab.com/blog/matlab-callbacks-for-java-events-in-r2014a

--- software/examples/matlab/matlab_example_beep.m
+++ software/examples/matlab/matlab_example_beep.m
@@ -7,7 +7,7 @@ function matlab_example_beep()
     UID = 'XYZ'; % Change to your UID
 
     ipcon = IPConnection(); % Create IP connection
-    ps = BrickletPiezoSpeaker(UID, ipcon); % Create device object
+    ps = handle(BrickletPiezoSpeaker(UID, ipcon), 'CallbackProperties'); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
