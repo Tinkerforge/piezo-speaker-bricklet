6c074d75dda84298be6459ebcf329c68b35a5a2e
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:14:01 +0100

    Add go examples.

---
+++ software/examples/go/example_morse_code.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/piezo_speaker_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Piezo Speaker Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	ps, _ := piezo_speaker_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Morse SOS with a frequency of 2kHz
+	ps.MorseCode("... --- ...", 2000)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
