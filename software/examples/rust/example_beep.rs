b17e2330a2a30711196a9ddbd160e3340a4aba55
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:38:58 +0100

    rust: Reformat examples.

--- software/examples/rust/example_beep.rs
+++ software/examples/rust/example_beep.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, io};
+use std::{io, error::Error};
 
-use tinkerforge::{ip_connection::IpConnection, piezo_speaker_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, 
+                  piezo_speaker_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -11,10 +13,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let ps = PiezoSpeakerBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Make 2 second beep with a frequency of 1kHz
-    ps.beep(2000, 1000);
+		// Make 2 second beep with a frequency of 1kHz
+		ps.beep(2000, 1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_morse_code.rs
+++ software/examples/rust/example_morse_code.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, io};
+use std::{io, error::Error};
 
-use tinkerforge::{ip_connection::IpConnection, piezo_speaker_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, 
+                  piezo_speaker_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -11,10 +13,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let ps = PiezoSpeakerBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Morse SOS with a frequency of 2kHz
-    ps.morse_code("... --- ...".to_string(), 2000);
+		// Morse SOS with a frequency of 2kHz
+		ps.morse_code("... --- ...".to_string(), 2000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
